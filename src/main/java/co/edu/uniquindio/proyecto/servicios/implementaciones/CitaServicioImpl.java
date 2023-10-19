9643b6d2f1d87d767fc82623cc072b31504b275b
juan sebastian garcia <juans.garciaa@uqvirtual.edu.co>
2023-10-19 01:32:30 -0500

    servicio de la clase comentario y su test, completado

--- src/main/java/co/edu/uniquindio/proyecto/servicios/implementaciones/CitaServicioImpl.java
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/implementaciones/CitaServicioImpl.java
@@ -2,11 +2,14 @@ package co.edu.uniquindio.proyecto.servicios.implementaciones;
 
 import co.edu.uniquindio.proyecto.entidades.Cita;
 import co.edu.uniquindio.proyecto.repositorios.CitaRepository;
+import co.edu.uniquindio.proyecto.servicios.excepciones.AttributeException;
 import co.edu.uniquindio.proyecto.servicios.interfaces.CitaServicio;
 import jakarta.transaction.Transactional;
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Service
@@ -23,26 +26,45 @@ public class CitaServicioImpl implements CitaServicio {
 
     @Override
     public Integer registrarCita(Cita a) throws Exception {
-        return null;
+        return repository.save(a).getId();
     }
 
     @Override
     public Integer actualizarCita(Cita a) throws Exception {
-        return null;
+
+        Optional<Cita> buscado=repository.findById(a.getId());
+
+        if(buscado.isEmpty()){
+            throw new AttributeException("la cita no se encuentra");
+        }
+
+
+        return repository.save(a).getId();
     }
 
     @Override
     public String eliminarCita(int id) throws Exception {
-        return null;
+        Optional<Cita> buscado=repository.findById(id);
+
+        if(buscado.isEmpty()){
+            throw new AttributeException("la cita no se encuentra");
+        }
+        repository.delete(buscado.get());
+        return "se elimino";
     }
 
     @Override
     public Cita buscarCita(int id) throws Exception {
-        return null;
+        Optional<Cita> buscado=repository.findById(id);
+
+        if(buscado.isEmpty()){
+            throw new AttributeException("la cita no se encuentra");
+        }
+        return buscado.get();
     }
 
     @Override
     public List<Cita> listarCita() {
-        return null;
+        return repository.findAll();
     }
 }
