ab6ba3a0ab1f9b7677f7b65d1b0cd05d2648c2a6
juan sebastian garcia <juans.garciaa@uqvirtual.edu.co>
2023-10-19 00:51:14 -0500

    servicio de atencion y test, completado

--- src/main/java/co/edu/uniquindio/proyecto/repositorios/AtencionRepository.java
+++ src/main/java/co/edu/uniquindio/proyecto/repositorios/AtencionRepository.java
@@ -1,10 +1,15 @@
 package co.edu.uniquindio.proyecto.repositorios;
 
+import co.edu.uniquindio.proyecto.entidades.Alergia;
 import co.edu.uniquindio.proyecto.entidades.Atencion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AtencionRepository extends JpaRepository<Atencion,Integer> {
+
+    Optional<Atencion> findByCitaId(Integer citaId);
 
 }
--- src/main/java/co/edu/uniquindio/proyecto/servicios/implementaciones/AtencionServicioImpl.java
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/implementaciones/AtencionServicioImpl.java
@@ -2,11 +2,13 @@ package co.edu.uniquindio.proyecto.servicios.implementaciones;
 
 import co.edu.uniquindio.proyecto.entidades.Atencion;
 import co.edu.uniquindio.proyecto.repositorios.AtencionRepository;
+import co.edu.uniquindio.proyecto.servicios.excepciones.AttributeException;
 import co.edu.uniquindio.proyecto.servicios.interfaces.AtencionServicio;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Service
@@ -24,26 +26,57 @@ public class AtencionServicioImpl implements AtencionServicio {
 
     @Override
     public Integer registrarAtencion(Atencion a) throws Exception {
-        return null;
+
+        Optional<Atencion> buscado=repository.findByCitaId(a.getCitaId());
+
+        if(!buscado.isEmpty()){
+            throw new AttributeException("la cita ya paso por una consulta");
+        }
+
+        return repository.save(a).getId();
     }
 
     @Override
     public Integer actualizarAtencion(Atencion a) throws Exception {
-        return null;
+
+        Optional<Atencion> buscado=repository.findByCitaId(a.getCitaId());
+
+        if(buscado.isEmpty()){
+            throw new AttributeException("no se encuentra la atencion");
+        }
+
+
+        return repository.save(a).getId();
     }
 
     @Override
     public String eliminarAtencion(int id) throws Exception {
-        return null;
+
+        Optional<Atencion> buscado=repository.findByCitaId(id);
+
+        if(buscado.isEmpty()){
+            throw new AttributeException("no se encuentra la atencion");
+        }
+
+        repository.delete(buscado.get());
+
+        return "se elimino";
     }
 
     @Override
     public Atencion buscarAtencion(int id) throws Exception {
-        return null;
+
+        Optional<Atencion> buscado=repository.findByCitaId(id);
+
+        if(buscado.isEmpty()){
+            throw new AttributeException("no se encuentra la atencion");
+        }
+
+        return buscado.get();
     }
 
     @Override
     public List<Atencion> listarAtencion() {
-        return null;
+        return repository.findAll();
     }
 }
