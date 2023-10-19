2b3f7f89cc905a568cdbd2990353b0ca91777f8b
juan sebastian garcia <juans.garciaa@uqvirtual.edu.co>
2023-10-18 21:33:15 -0500

    el test de administrador quedo funcionando correctamente, ahora se debe comenzar con la construccion de los servicios

--- src/test/java/co/edu/uniquindio/proyecto/test/AdministradorTest.java
+++ src/test/java/co/edu/uniquindio/proyecto/test/AdministradorTest.java
@@ -2,6 +2,7 @@ package co.edu.uniquindio.proyecto.test;
 
 import co.edu.uniquindio.proyecto.entidades.Administrador;
 import co.edu.uniquindio.proyecto.repositorios.AdministradorRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -17,18 +18,20 @@ public class AdministradorTest {
     private AdministradorRepository repo;
 
     @Test
+    @Transactional
     public void registrarAdministrador(){
 
         Administrador admin=new Administrador();
 
-        admin.setCedula(123);
-        admin.setNombre("juan");
+        admin.setCedula(1234);
+        admin.setNombre("carlos");
         admin.setEmail("juan@");
         admin.setAcceso(3);
 
         repo.save(admin);
-        Optional<Administrador> buscado=repo.findById(123);
 
+
+        Optional<Administrador> buscado=repo.findById(1234);
         System.out.println(buscado.get().getNombre());
     }
 
